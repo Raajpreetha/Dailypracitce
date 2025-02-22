483d4f3fd057b757498de82a6cf60dec4a7ee63c
preethamahadev@gmail.com <preethamahadev@gmail.com>
2025-02-22 23:29:25 +0530

    newupdate

--- Day3and4/scope.js
+++ Day3and4/scope.js
@@ -1,8 +1,18 @@
-var x=10; // global scope
-function display()
-{
-    var x=30; // local scope
-console.log(x);
+// var x=10; // global scope
+// function display()
+// {
+ //   var x=30; // local scope
+//console.log(x);
+ //}
+//display();
+//console.log(x);
+// 
+var x =10;
+function display(){
+    var x= 200;
+    var y = 30;
+    console.log(x);
+    console.log(y);
 }
 display();
-console.log(x);
+console.log(y);
